5752c8c176246485f4040052107053f3346b42aa
Jonathan Howard Kaufman <jonathan.kaufman@quantumburgh.com>
2025-01-31 12:48:37 -0500

    simplifying the solution

--- chargers.py
+++ chargers.py
@@ -1,15 +1,30 @@
 # chargers.py
 
-import numpy as np
-import dimod
-from typing import Tuple, Set
-from numpy.typing import NDArray
+# import numpy as np
+# import dimod
+from math import sqrt
+from typing import Tuple, Set, List
+# from numpy.typing import NDArray
 
 width: int = 256
 height: int = 256
-grid: NDArray[np.float64] = np.zeros((width, height), dtype=np.float64)
-elocs: Set[Tuple] = {(5, 5), (25, 10), (200, 75)} #existing locations
+locs: Set[Tuple] = {(5, 5), (25, 10), (200, 75)} #existing locations
+pois: Set[Tuple] = {(20, 200), (200, 200)} #existing points of interest
 numnew: int = 2 #number of new chargers
-solution: Set[Tuple] = {}
+solution: Set[Tuple] = set() #container for new locations
+
+def distance(a: Tuple[int], b: Tuple[int]) -> float:
+  return sqrt((float(a[0]-b[0])**2) + (float(a[1]-b[1])**2))
+
+solution.add((1,2))
+
+print(solution)
 
 
+def test_distance():
+  if distance((5, 5), (6, 6)) - 1.4 < 0.1:
+    print("distance passes")
+  else:
+    print("distance fails")
+    
+test_distance()
