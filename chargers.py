ff0eb4882f99222332c7f3ff87ebc729a047e8c1
Jonathan Howard Kaufman <jonathan.kaufman@quantumburgh.com>
2025-02-03 15:18:28 -0500

    Update chargers.py

--- chargers.py
+++ chargers.py
@@ -32,8 +32,9 @@ def guess(n: int) -> int:
   
 #update state by adding random location
 def addrandcs(cs: Set[Tuple], w: int, h: int) -> Set[Tuple]:
-  return cs.add((guess(w),guess(h)))
-
+  cscopy = cs.copy()
+  cscopy.add((guess(w),guess(h)))
+  return cscopy
 
 #initial conditions
 width: int = 32
@@ -42,40 +43,54 @@ pois: Set[Tuple] = {(5, 5), (25, 10), (20, 30)} #existing points of interest
 numnew: int = 2 #number of new chargers
 locs: Set[Tuple] = set() #state variable location of charging stations
 
-
 #monte-carlo technique
 iterations: int = (width*height)**2
-
-
-
-newcs = addrandcs(
+locs = addrandcs(locs, width, height) #add a charging station updating state variable
 
 #solution
-solution: Set[Tuple] = set() #container for new locations
-solution.add((1,2))
-print(solution)
+print()
+print("Solution:")
+print(locs)
+print()
 
-
+#function tests
 def test_distance():
   if distance((5, 5), (6, 6)) - 1.4 < 0.1:
-    print("distance passes")
+    print("distance() passes")
   else:
-    print("distance fails")
+    print("distance() fails")
+  return 0
     
 def test_closest():
   answer: float = closest((2,3),{(3,4),(5,5),(4,6)})
   if answer - 1.4 < 0.1:
-    print("closest passes")
+    print("closest() passes")
   else:
-    print("closest fails")
+    print("closest() fails")
+  return 0
     
 def test_avgmindist():
   answer: float = avgmindist({(2,3),(4,4)},{(3,4),(5,5),(4,6)})
   if answer - 1.2 < 0.1:
-    print("avgmindist passes")
+    print("avgmindist() passes")
   else:
-    print("avgmindist fails")
+    print("avgmindist() fails")
+  return 0
     
+def test_addrandcs():
+  s = set()
+  ss = addrandcs(s,10,10)
+  sss = next(iter(ss))[0]
+  answer = 0 <= sss < 10
+  if answer == True:
+    print("addrandcs() passes")
+  else:
+    print("addrandcs() fails")
+  return 0
+   
+print("Function tests:") 
 test_distance()
 test_closest()
 test_avgmindist()
+test_addrandcs()
+print()
